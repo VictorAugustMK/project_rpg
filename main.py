f109848439910f1502020723e75112b8a52d297d
Victor <victor.macc12@gmail.com>
2022-07-31 16:53:48 -0300

    Criação de repetição das funções

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import sys
+import pandas as pd
+
 from lista import pericias as ps
 from loguru import logger
-import pandas as pd
 
 
 class ficha:
 
-    def selects(self):
-        select_lvl = (input('Nivel desejado: '))
+    def selects_lvl(self):
+        ok = False
+        lvl = 0
+        while True:
+            select_lvl = (input('Nivel desejado: '))
+            if select_lvl.isnumeric():
+                lvl = int(select_lvl)
+                ok = True
+            else:
+                print('\033[0;31mERRO! Digite apenas números.\033[m')
+            if ok:
+                break
+        return lvl
 
-        pericias = pd.DataFrame(ps, copy=False)
-        select_peri = (input(f'Seleciona uma periacia: {pericias}'))
+    def select_expertise(self):
 
-        print(f'Nivel: {select_lvl} \n'
-              f'Pericias: {select_peri}')
+        expert = False
+        lvl = ''
+        while True:
+            expertise = pd.DataFrame(ps, copy=False)
+            select_expert = (input(f'Seleciona uma periacia: {expertise}'))
 
+            if select_expert in ps:
+                lvl = select_expert
+                expert = True
+            else:
+                print('\033[0;31mERRO! Digite alguma pericia acima.\033[m')
+            if expert:
+                break
+        return lvl
 
 if __name__ == "__main__":
     query = ficha()
-    query.selects()
+
+    query.selects_lvl()
+    query.select_expertise()
